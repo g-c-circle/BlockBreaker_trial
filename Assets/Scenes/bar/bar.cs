aa1b03784ec5066d7cc13d1142d0e7f3943c34e7
2321025-PC\2321025 <donbei796@gmail.com>
2023-12-12 13:34:17 +0900

    変更

--- Assets/Scenes/bar/bar.cs
+++ Assets/Scenes/bar/bar.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class bar : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float movespeed = 0.02f;
+    public float movespeed = 0.0002f;
+    public float v = 10f;
     private float vInput;
     public float bx=0f;
     public bool stay=false;
     void OnTriggerStay(Collider other) {
         if (!stay) stay = true;
             transform.position= new Vector3(bx, 0f, 0f);
-
+        Debug.Log("stay");
     }
     void OnTriggerEnter(Collider collider) {
         if (!stay) stay = true;
@@ -29,11 +30,11 @@ public class bar : MonoBehaviour
     {
         if (!stay){
             bx = transform.position.x;
-            Debug.Log(bx);
+           // Debug.Log(bx);
         }
-    vInput = Input.GetAxis("Horizontal");
-        Debug.Log(vInput*Time.deltaTime);
-    transform.Translate(Vector3.right * vInput*movespeed);
+    vInput = Input.GetAxis("Horizontal")*movespeed;
+       // Debug.Log(vInput*Time.deltaTime);
+    transform.Translate(Vector3.right*vInput*v*Time.deltaTime);
 
         /*if (Input.GetKey(KeyCode.A))
             transform.Translate(-1f, 0f, 0f, Space.World);
