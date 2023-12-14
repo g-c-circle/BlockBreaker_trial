8e50a87cdb48300cf433298b8ab91d32ee232ed1
2321107-PC\2321107 <happy.yuuki.ao@gmail.com>
2023-12-14 20:43:30 +0900

    block 少し修正

--- Assets/block/block script.cs
+++ Assets/block/block script.cs
@@ -40,6 +40,29 @@ public class BlockScript : MonoBehaviour
         }
     }
 
+    void GenerateBlocksRestart()
+    {
+        int x, y;
+        for (x = -10; x < 15; x += 5)
+        {
+            for (y = 20; y > 0; y -= 5)
+            {
+                GameObject block = Instantiate(BlockObject, new Vector3(x, y, 0), Quaternion.Euler(blockRotation));
+                block.transform.localScale = currentSize;
+
+                // ブロックの色を設定
+                Renderer renderer = block.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    Material material = new Material(renderer.material);
+                    material.color = blockColor;
+                    renderer.material = material;
+                }
+            }
+        }
+    }
+
+
     // ブロックがボールに触れたら作動する関数
     private void OnCollisionEnter(Collision collision)
     {
@@ -58,7 +81,7 @@ public class BlockScript : MonoBehaviour
             blockColor = new Color(Random.value, Random.value, Random.value);//色変数を変更(ランダム)
             currentSize += new Vector3(0.1f, 0.1f, 0.1f);//大きさ変数を変更(少し大きく)
             blockRotation = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));//角度変数を変更(ランダム)
-            GenerateBlocks(); // ブロックを再生成して新しい大きさを反映
+            GenerateBlocksRestart(); // ブロックを再生成して新しい大きさを反映
         }
     }
 }
