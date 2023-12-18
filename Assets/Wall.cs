4a8c4975c30cc943d41f8a0c1e4d15f0e6938e63
2323179-PC\2323179 <bitshin777@icloud.com>
2023-12-18 18:20:36 +0900

    Wall12/18

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall : MonoBehaviour
+public class wall : MonoBehaviour
 {
+    public float reflectSpeed = 1.0f;
+    public float maxSpeedUps = 3;
+    public float speedUpCount = 0;
+    //public bool isColliding = false;サンプル３
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
@@ -24,38 +27,68 @@ public class Wall : MonoBehaviour
             Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
             if (ballRb != null)
             {
+                /*
                 Vector3 currentVelocity = ballRb.velocity;
-                Vector3 reflection = Vector3.Reflect(currentVelocity, collision.contacts[0].normal);
-                //ballRb.velocity = reflection;
-                reflection.y = currentVelocity.y;
-                ballRb.velocity = reflection * 2.0f;
-            }
-        }
+                Vector3 reflection = Vector3.Reflect(currentVelocity, collision.contacts[0].normal).normalized * reflectSpeed;
+                */
 
-
-    }
-    
-    /*
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ball"))
-        {
-            Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
-            if (ballRb != null)
-            {
-                //Vector3 currentVelocity = ballRb.velocity;
-                Vector3 forceDirection = transform.position - collision.contacts[0].point;
                 //ballRb.velocity = reflection;
                 //reflection.y = currentVelocity.y;
-                ballRb.AddForce(forceDirection * 100f, ForceMode.Force);
+                // ballRb.velocity = reflection;
+
+
+                //これかサンプル1
+                if (speedUpCount < maxSpeedUps)
+                {
+                    ballRb.velocity *= reflectSpeed;
+                    Vector3 reflection = Vector3.Reflect(ballRb.velocity, collision.contacts[0].normal).normalized;
+                    ballRb.AddForce(reflection * reflectSpeed, ForceMode.Impulse);
+                    speedUpCount++;
+                }
+
+
+                /*これサンプル2
+                if (speedUpCount < maxSpeedUps)
+                {
+                    Vector3 reflection = Vector3.Reflect(ballRb.velocity, collision.contacts[0].normal).normalized;
+                    ballRb.velocity = reflection * reflectSpeed * ballRb.velocity.magnitude;
+                    speedUpCount++;
+                }*/
+
+
             }
         }
 
 
     }
-    */
+
+    /*void OnCollisionExit(Collision collision)サンプル3
+     {
+         if (collision.gameObject.CompareTag("Ball"))
+         {
+             isColliding = false;
+         }
+     }
+
+     void OnCollisionEnter(Collision collision)
+     {
 
 
+         if (!isColliding && collision.gameObject.CompareTag("Ball"))
+         {
+             isColliding = true;
 
-
+             Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
+             if (ballRb != null)
+             {
+                 if (speedUpCount < maxSpeedUps)
+                 {
+                     ballRb.velocity *= reflectSpeed;
+                     Vector3 reflection = Vector3.Reflect(ballRb.velocity, collision.contacts[0].normal).normalized;
+                     ballRb.AddForce(reflection * reflectSpeed, ForceMode.Impulse);
+                     speedUpCount++;
+                 }
+             }
+         }
+     }*/
 }
