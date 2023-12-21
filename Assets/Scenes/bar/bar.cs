d38f42b7cfe52aa5721a2eb92d2310122ccc44aa
2321025-PC\2321025 <donbei796@gmail.com>
2023-12-21 19:08:44 +0900

    横判定完成・スクリプト文書校正・オブジェクト、スクリプトのテスト用の振り分け

--- Assets/Scenes/bar/bar.cs
+++ Assets/Scenes/bar/bar.cs
@@ -6,49 +6,57 @@ using UnityEngine;
 public class bar : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float movespeed = 50f;
-    public float v = 10f;
-    private float vInput;
-    public float bx=0f;
-    public bool stay=false;
-    public Rigidbody ball;
-    private float balls = 1.01f;
-    void OnTriggerStay(Collider other) {
-        if (!stay) stay = true;
-            transform.position= new Vector3(bx, 0f, 0f);
-        Debug.Log("stay");
-      
-    }
-    void OnTriggerEnter(Collider collider){ 
-        if (!stay) stay = true;
-        transform.position = new Vector3(bx, 0f, 0f);
-        ball = collider.gameObject.GetComponent<Rigidbody>();
-        Debug.Log(ball.velocity);
-        balls += 0.001f;
-        Debug.Log(balls);
-        ball.velocity = new Vector3(balls*ball.velocity.x,(ball.velocity.y*-1)*balls, 0);
-        //ball.velocity = new Vector3(ball.velocity.x*1.01f, (ball.velocity.y * -1) * 1.01f,0);
-        Debug.Log(ball.velocity);
-    }
-    void OnTriggerExit(Collider other){
-        stay = false;
-        //Debug.Log("exit");
-    }
+    public float movespeed = 1f;
     
-    // Update is called once per frame
-    void Update()
-    {
-        if (!stay){
-            bx = transform.position.x;
-           // Debug.Log(bx);
-        }
-    vInput = Input.GetAxis("Horizontal")*v;
-       // Debug.Log(vInput*Time.deltaTime);
-    transform.Translate(Vector3.right*vInput*movespeed*Time.deltaTime);
+    public float vInput;
+    
+    public float bx=0f; //bar's position before colliding wall.
+    
+    public bool stay=false; // whether bar is colliding wall.
+    
+    public Rigidbody ball;
 
-        /*if (Input.GetKey(KeyCode.A))
-            transform.Translate(-1f, 0f, 0f, Space.World);
-        if (Input.GetKey(KeyCode.D))
-            transform.Translate(1f, 0f, 0f, Space.World);*/
+    public float addspeed = 1.01f;
+
+    void back_before_stay() {
+
+        if (!stay) stay = true;
+
+        transform.position = new Vector3(bx, 0f, 0f);
     }
+
+    virtual public void OnTriggerStay(Collider other) {
+
+        back_before_stay();
+
+    }
+    virtual public void OnTriggerEnter(Collider collider){
+        back_before_stay();
+
+        ball = collider.gameObject.GetComponent<Rigidbody>();
+
+        addspeed += 0.001f;
+
+        ball.velocity = new Vector3(addspeed*ball.velocity.x,(ball.velocity.y*-1)*addspeed, 0);
+
+    }
+
+    virtual public void OnTriggerExit(Collider other){
+
+        stay = false;
+    }
+
+    virtual public void move() {
+        if (!stay)
+        {
+            bx = transform.position.x;//recorting bar x position that bar does't colliding to wall.
+        }
+        vInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * vInput * movespeed * Time.deltaTime);
+    }
+    // Update is called once per frame
+     void Update()
+     {
+        move();
+     }
 }
